b125f97685ab563ffeb79f7e12a7a6386482452d
moksh-mahajan <mokshmahajan008@gmail.com>
2019-07-19 12:39:34 +0530

    Added JVMStatic annotations and minSDKVersion set to 15

--- fpauthdialoglibrary/src/main/java/com/github/mokshmahajan/fpauthdialoglibrary/FpAuthSupport.kt
+++ fpauthdialoglibrary/src/main/java/com/github/mokshmahajan/fpauthdialoglibrary/FpAuthSupport.kt
@@ -6,15 +6,18 @@ import android.support.v4.hardware.fingerprint.FingerprintManagerCompat
 
 object FpAuthSupport {
 
+    @JvmStatic
     fun checkAvailability(context: Context): Boolean {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
                 FingerprintManagerCompat.from(context).isHardwareDetected
     }
 
+    @JvmStatic
     fun isFingerprintRegistered(context: Context): Boolean {
         return FingerprintManagerCompat.from(context).hasEnrolledFingerprints()
     }
 
+    @JvmStatic
     fun checkAvailabiltyAndIfFingerprintRegistered(context: Context): Boolean {
         val fingerprintManagerCompat = FingerprintManagerCompat.from(context)
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
